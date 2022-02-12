19ff6fd8ce4620ce3c696a2aedebdf19907244bd
Baptiste LEGO <blego@lithium.paris>
2022-02-12 23:26:02 +0100

    wip q3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import org.apache.spark.sql.functions._
 
 
 FILENAME = "full.csv"
@@ -12,3 +13,9 @@ df = spark.read.csv(FILENAME)
 
 # REPONSE 2
 #df.filter(df._c4 == "apache/spark").groupBy("_c1").count().orderBy("count", ascending=False).show(1)
+
+# REPONSE 3
+#df.filter(df._c4 == "apache/spark").groupBy("_c1").count().orderBy("count", ascending=False).show()
+df.select(col("_c0"), month_between(current_date(),col("_c2")))
+
+#df.show()
